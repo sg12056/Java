5a5d02953f4eae1b4634c23eed1c2b94b4ba767b
sg12056 <gupta_saurabh@optum.com>
2019-06-14 14:29:49 +0530

    Spring Security Added

--- src/restConfig/ApplicationConfiguration.java
+++ src/restConfig/ApplicationConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "restController")
+@ComponentScan({"restController","securityConfig"})
 public class ApplicationConfiguration {     
  ApplicationConfiguration() {
 	 System.out.println("ApplicationConfiguration");
