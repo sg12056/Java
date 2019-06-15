41ca5cbc09b94f3c01e56384974a18265730a9b1
sg12056 <gupta_saurabh@optum.com>
2019-06-15 16:22:51 +0530

    Project Structure Changed,Views Added

--- src/restConfig/ApplicationConfiguration.java
+++ src/restConfig/ApplicationConfiguration.java
@@ -1,14 +1,144 @@
 package restConfig;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
- 
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.ResourceBundleViewResolver;
+
 @Configuration
 @EnableWebMvc
-@ComponentScan({"restController","securityConfig"})
-public class ApplicationConfiguration {     
- ApplicationConfiguration() {
-	 System.out.println("ApplicationConfiguration");
- }
+@ComponentScan({ "restController", "securityConfig"})
+public class ApplicationConfiguration implements WebMvcConfigurer {
+	ApplicationConfiguration() {
+		System.out.println("ApplicationConfiguration");
+	}
+
+	@Bean
+	public ViewResolver internalResourceViewResolver() {
+    InternalResourceViewResolver bean = new InternalResourceViewResolver();
+    bean.setViewClass(JstlView.class);
+	bean.setPrefix("WEB-INF/views/");
+	// bean.setPrefix("WEB-INF/views/resources/**");
+	// bean.setPrefix("WEB-INF/views/resources/");
+    bean.setSuffix(".html");
+    return bean;
+	}
+
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	}
+
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+	}
+
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+	}
+
+	@Override
+	public Validator getValidator() {
+		return null;
+	}
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+	}
+
+	@Override
+	public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+
+	}
+
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+
+	}
+
+	@Override
+	public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
+
+	}
+
+	@Override
+	public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+
+	}
+
+	@Override
+	public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+
+	}
+
+	@Override
+	public MessageCodesResolver getMessageCodesResolver() {
+		return null;
+	}
+
+	
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+
+	}
 }
--- src/restController/PaymentController.java
+++ src/restController/PaymentController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import restResources.BaseResponse;
 import restResources.PaymentRequest;
@@ -14,59 +15,34 @@ import restResources.PaymentRequest;
 public class PaymentController {
 
 	private final String sharedKey = "Key";
-	private final String User = "user";
-	private final String Pass = "password";
+	//private final String User = "user";
+	//private final String Pass = "password";
 	private static final String SUCCESS_STATUS = "success";
 	private static final String ERROR_STATUS = "error";
 	private static final int CODE_SUCCESS = 100;
 	private static final int AUTH_FAILURE = 102;
 
-	@RequestMapping(value = "/pay", method = RequestMethod.POST)
-	public BaseResponse pay(@RequestParam(value = "key") String key, @RequestBody PaymentRequest request) {
-		System.out.println("PaymentController");
-		BaseResponse response = new BaseResponse();
-		if (sharedKey.equalsIgnoreCase(key)) {
-
-			// Process the request
-			// ....
-			// Return success response to the client.
-
-			response.setStatus(SUCCESS_STATUS);
-			response.setCode(CODE_SUCCESS);
-		} else {
-			response.setStatus(ERROR_STATUS);
-			response.setCode(AUTH_FAILURE);
-		}
+	@RequestMapping(value = "/pay", method = RequestMethod.GET)
+	public ModelAndView pay() {
+		System.out.println("Pay");
+		ModelAndView response = new ModelAndView();
+		response.setViewName("resources/pay");
 		return response;
 	}
 	
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public BaseResponse login( @RequestBody PaymentRequest request) {
-		System.out.println("PaymentController");
-		BaseResponse response = new BaseResponse();
-		String user = request.getUser();
-		String pass = request.getPass();
-		if (User.equalsIgnoreCase(user) && Pass.equalsIgnoreCase(pass)) {
-
-			// Process the request
-			// ....
-			// Return success response to the client.
-			response.setMessage("Logged In");
-			response.setStatus(SUCCESS_STATUS);
-			response.setCode(CODE_SUCCESS);
-		} else {
-			response.setStatus(ERROR_STATUS);
-			response.setCode(AUTH_FAILURE);
-			response.setMessage("Invalid UserName/Password");
-		}
-		return response;
-	}
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public BaseResponse welcome( ) {
-		System.out.println("PaymentController");
+		System.out.println("welcome");
 		BaseResponse response = new BaseResponse();
 		response.setMessage("Spring Frame Works!!");
 		return response;
-	}
+	 }
+	// @RequestMapping(value = "/login", method = RequestMethod.POST)
+	// public ModelAndView login( ) {
+	// 	System.out.println("login");
+	// 	ModelAndView response = new ModelAndView();
+	// 	response.setViewName("login");
+	// 	return response;
+	// }
 
 }
