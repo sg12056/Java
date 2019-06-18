a3a2afbc56a3284d8ba02b24a9349aa880431f15
sg12056 <gupta_saurabh@optum.com>
2019-06-18 13:31:56 +0530

    Spring version changed

--- src/main/restController/jdbcController.java
+++ src/main/restController/jdbcController.java
@@ -5,15 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class jdbcController extends JdbcTemplate {
+public class jdbcController {
    
     @RequestMapping("/query")
     private int query(){
         
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(getDataSource());
-        int row = jdbcTemplate.queryForObject("Select count(*) from localdb.employee"
-        ,Integer.class);
-        return row;
+        // JdbcTemplate jdbcTemplate = new JdbcTemplate(getDataSource());
+        // int row = jdbcTemplate.queryForObject("Select count(*) from localdb.employee"
+        // ,Integer.class);
+         return 0;
     }
     
 }
